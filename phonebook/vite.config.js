020605dbfd4a9dbbfd5f01c6d0a49fcb6a2f5b18
Khushi Yadav <khushi.3050@iitg.ac.in>
2026-06-04 16:47:04 +0530

    Fix API URLs

--- phonebook/vite.config.js
+++ phonebook/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
    server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://phonebook-fullstack-rhu4.onrender.com/api/persons',
         changeOrigin: true,
       },
     }
