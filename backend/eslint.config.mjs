72ea36ad930b044aff28d99e26f7a2bc8f9deb32
Khushi Yadav <khushi.3050@iitg.ac.in>
2026-06-11 22:08:49 +0530

    gitignore mongo.js

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,23 @@
+// import js from "@eslint/js";
+// import globals from "globals";
+// import pluginReact from "eslint-plugin-react";
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+//   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+//   pluginReact.configs.flat.recommended,
+// ]);
+
+import globals from 'globals'
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+  },
+]
